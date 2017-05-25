e9abf539198a364ba36ba31f885873e7aaa0dc72
Wassim S Seifeddine <wassim_seifeddine95@live.com>
2017-05-25 18:41:39 +0300

    Initial Commit

---
+++ Sourcery-Templates/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Sourcery-Templates
+//
+//  Created by Wassim Seifeddine on 5/25/17.
+//  Copyright © 2017 Wassim Seifeddine. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
